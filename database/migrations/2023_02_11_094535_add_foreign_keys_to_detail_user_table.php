d16829411c8b1381621f00e8c563991ab7b58737
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2023-02-11 17:47:43 +0700

    finish add foreign key

---
+++ database/migrations/2023_02_11_094535_add_foreign_keys_to_detail_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_detail_user_to_users')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('type_user_id', 'fk_detail_user_to_type_user')->references('id')->on('type_user')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('detail_user', function (Blueprint $table) {
+            $table->dropForeign('fk_detail_user_to_users');
+            $table->dropForeign('fk_detail_user_to_type_user');
+        });
+    }
+};
