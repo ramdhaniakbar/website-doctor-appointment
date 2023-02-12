d22136107b5e44aed8de380c56346ad4c92fb4dd
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2023-02-12 11:20:54 +0700

    load assets and slicing frontsite

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Frontsite\AppointmentController;
+use App\Http\Controllers\Frontsite\LandingController;
+use App\Http\Controllers\Frontsite\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +16,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::resource('/', LandingController::class);
+
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+    // appointment page
+    Route::resource('appointment', AppointmentController::class);
+
+    // payment page
+    Route::resource('payment', PaymentController::class);
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::group(['prefix' => 'backsite', 'as' => 'backsite.', 'middleware' => ['auth:sanctum', 'verified']], function () {
+    return view('dashboard');
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
